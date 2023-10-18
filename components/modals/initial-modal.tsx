a9f7d6cccf33307cb3b293d92c4e97469441394f
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2023-10-18 14:04:51 -0600

    ✅ Feat: HAdd image upload functionality

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -25,6 +25,7 @@ import {
 
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { FileUpload } from "@/components/ui/file-upload"
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -75,7 +76,21 @@ export const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                TODO: Image upload
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload 
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
 
               <FormField
