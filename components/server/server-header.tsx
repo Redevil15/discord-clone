7c8efd576d66bb96723f98ec907b0d4ab6c7c801
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2023-11-03 12:25:07 -0600

    Feat: Add leave server functionality an patch leave service

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -90,6 +90,7 @@ const ServerHeader = ({
 
         {!isAdmin && (
           <DropdownMenuItem
+            onClick={() => onOpen("leaveServer", { server })}
             className=" text-rose-500 px-3 py-2 text-sm cursor-pointer"
           >
             Leave Server
