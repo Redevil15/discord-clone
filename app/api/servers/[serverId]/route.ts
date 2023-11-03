b1669b58363d9a6ede65be63961f44aad0baf043
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2023-11-03 12:35:58 -0600

    Feat: Add deleted server functionality

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server"
 export async function PATCH(
   req: Request,
   { params }: { params: { serverId: string }}
-){
+) {
   try{
     const profile = await currentProfile();
     const { name, imageUrl } = await req.json()
@@ -29,6 +29,32 @@ export async function PATCH(
 
   }catch(error) {
     console.log("[SERVER_ID_PATCH]", error)
+    return new NextResponse("Internal Error", { status: 500})
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { serverId: string }}
+) {
+  try{
+    const profile = await currentProfile();
+    
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401})
+    }
+    
+    const server = await db.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+    })
+
+    return NextResponse.json(server)
+
+  }catch(error) {
+    console.log("[SERVER_ID_DELETE]", error)
     return new NextResponse("Internal Error", { status: 500})
   }
 }
