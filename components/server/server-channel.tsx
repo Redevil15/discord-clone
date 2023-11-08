21fcaacaf1dcfb9c16afb394cbadefb782df25e5
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2023-11-07 19:03:42 -0600

    Feat: Change universal onClick by an onAction function

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -11,7 +11,7 @@ import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { Tooltip } from "@/components/ui/tooltip";
 import { ActionTooltip } from "@/components/ui/action-tooltip";
-import { useModal } from "@/hooks/use-modal-store";
+import { ModalType, useModal } from "@/hooks/use-modal-store";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -35,10 +35,19 @@ const ServerChannel = ({
   const { onOpen } = useModal();
   const Icon = iconMap[channel.type]
 
+  const onClick = () => {
+    router.push(`/servers/${params?.serverId}/channels/${channel.id}`)
+  }
+
+  const onAction = (e: React.MouseEvent, action: ModalType) => {
+    e.stopPropagation();
+    onOpen(action, { server, channel });
+  }
+
 
   return (
     <button
-      onClick={() => { }}
+      onClick={onClick}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
@@ -58,13 +67,13 @@ const ServerChannel = ({
         <div className="ml-auto flex items-center gap-x-2">
           <ActionTooltip label="Edit">
             <Edit
-              onClick={() => onOpen("editChannel", { server, channel })}
+              onClick={(e) => onAction(e, "editChannel")}
               className="hidden group-hover:block h-4 w-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
             />
           </ActionTooltip>
           <ActionTooltip label="Delete">
             <Trash
-              onClick={() => onOpen("deleteChannel", { server, channel })}
+              onClick={(e) => onAction(e, "deleteChannel")}
               className="hidden group-hover:block h-4 w-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
             />
           </ActionTooltip>
