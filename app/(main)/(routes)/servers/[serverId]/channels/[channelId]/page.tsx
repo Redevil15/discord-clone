e8bfe662c820b5eed06adabc927e630473cf4352
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2023-11-19 08:43:22 -0600

    Feat: Add chatMessages component, review issue with tanstack loading state

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,6 @@
 import ChatHeader from '@/components/chat/chat-header';
 import ChatInput from '@/components/chat/chat-input';
+import { ChatMessages } from '@/components/chat/chat-messages';
 import { currentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
 import { redirectToSignIn } from '@clerk/nextjs';
@@ -45,12 +46,23 @@ const ChannelIdPage = async ({
         name={channel.name}
         type="channel"
       />
-      <div className="flex-1">
-        Future Mesagges
-      </div>
-      <ChatInput 
+      <ChatMessages
         name={channel.name}
-        type= "channel"
+        member={member}
+        chatId={channel.id}
+        apiUrl="/api/messages"
+        socketUrl="/api/socket/messages"
+        socketQuery={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+        paramKey="channelId"
+        paramValue={channel.id}
+        type="channel"
+      />
+      <ChatInput
+        name={channel.name}
+        type="channel"
         apiUrl="/api/socket/messages"
         query={{
           serverId: channel.serverId,
